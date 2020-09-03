994b12fa1df07f91d37fd336822c0fba27f372e5
Lakshmi Reddy Motati <lmotati@L-HYD012J9K8-M.lan>
2020-09-03 21:06:28 +0530

    added more products

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div>
       <ListRendering />
+      <ListRendering />
     </div>
   );
 }
--- src/Product.js
+++ src/Product.js
@@ -2,6 +2,7 @@ import React from "react";
 function Product({ product }) {
   return (
     <div>
+      <h2>Product Details</h2>
       <h2>Title : {product.title}</h2>
       <h3>Description : {product.description}</h3>
       <h4>Price : {product.price}</h4>
